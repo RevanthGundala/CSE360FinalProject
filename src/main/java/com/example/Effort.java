9ffac11d4cb208452b78f050e2143eed2c6efba2
Ben Stokman <bstokman@asu.edu>
2023-04-30 23:52:43 -0700

    Commented Effort.java

--- src/main/java/com/example/Effort.java
+++ src/main/java/com/example/Effort.java
@@ -21,39 +21,48 @@ public class Effort {
     public int getEffortId() {
         return effortId;
     }
-
+    
+    // Sets ID of effort object
     public void setEffortId(int effortId) {
         this.effortId = effortId;
     }
 
+    // Returns task ID
     public int getTaskId() {
         return taskId;
     }
 
+    // Set ID of task object
     public void setTaskId(int taskId) {
         this.taskId = taskId;
     }
 
+    // Get name of effort object
     public String getName(){
         return effortName;
     }
 
+    // Set name of effort object
     public void setName(String effortName){
         this.effortName = effortName;
     }
 
+    // Get description of effort object
     public String getDescription(){
         return effortDescription;
     }
 
+    // Set description of effort object
     public void setDescription(String effortDescription){
         this.effortDescription = effortDescription;
     }
 
+    // Get hours attributed to effort object
     public int geteffortHours() {
         return effortHours;
     }
 
+    // Sets hours attributed to effort object
     public void seteffortHours(int effortHours) {
         this.effortHours = effortHours;
     }
